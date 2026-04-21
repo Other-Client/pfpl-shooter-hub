0edd70dafd7c732bc59a9fc8a3c517731559f296
Zeeshan <zeeshan.zimension@gmail.com>
2026-04-21 19:06:44 +0530

    Another Env varibale fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SES_ACCESS_KEY_ID: process.env.SES_ACCESS_KEY_ID ?? "",
+    SES_SECRET_ACCESS_KEY: process.env.SES_SECRET_ACCESS_KEY ?? "",
+    SES_REGION: process.env.SES_REGION ?? "",
+    EMAIL_FROM: process.env.EMAIL_FROM ?? "",
+    EMAIL_REPLY_TO: process.env.EMAIL_REPLY_TO ?? "",
+  },
 };
 
 export default nextConfig;
